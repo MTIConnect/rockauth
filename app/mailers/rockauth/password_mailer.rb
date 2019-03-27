4150ff588e8288ee4e12de8889f264142481cc59
Janice Laset-Parkerson <janicelaset@gmail.com>
2019-03-27 10:05:35 -0700

    MTIC-2212 Fix issue with missing subdomain when origin header is used

--- app/mailers/rockauth/password_mailer.rb
+++ app/mailers/rockauth/password_mailer.rb
@@ -9,8 +9,15 @@ class Rockauth::PasswordMailer < ActionMailer::Base
   end
 
   def reset_password_link
-    url_params = [:edit, @resource_owner.model_name.param_key, :session_password, user: { password_reset_token: @token }, subdomain: @subdomain]
-    url_params.last[:host] = @origin if @subdomain.nil? && @origin.present?
+    subdomain = @subdomain
+    host = App.domain
+    if subdomain.nil? && @origin.present?
+      host = @origin
+      url_array = URI.parse(@origin).host.split('.')
+      url_array.pop(2)
+      subdomain = url_array.join('.')
+    end
+    url_params = [:edit, @resource_owner.model_name.param_key, :session_password, user: { password_reset_token: @token }, subdomain: subdomain, host: host]
     url_for(url_params)
   end
 end
